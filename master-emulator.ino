4ffa68cca13b93ff2bee08ff0a2261f049a7c876
Gioele <48024736+Gioee@users.noreply.github.com>
2023-11-21 12:46:27 +0100

    Added control from real fencers (only epee)

--- master-emulator.ino
+++ master-emulator.ino
@@ -37,6 +37,17 @@ Thanks for some of these precious information to https://www.reddit.com/r/Fencin
 
 #include <SoftwareSerial.h>  //https://docs.arduino.cc/learn/built-in-libraries/software-serial
 
+#define msHitTime 5
+#define msDoubleTime 40
+#define msHitLampDelay 1200
+#define msGNDLampDelay 0
+
+#define supHit 600
+#define extGND 300
+#define aGND 400
+#define v5Drop 1000
+#define realGND 100
+
 #define rxPin 10
 #define txPin 11
 #define msgLength 10
@@ -46,81 +57,132 @@ byte outMsg[msgLength];
 
 SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
 unsigned long currentTime, prevTime;
+unsigned long lxATime, rxATime, lxGNDTime, rxGNDTime;
 unsigned long redTime, greenTime, lYellowTime, rYellowTime;
 bool red, green, lYellow, rYellow, lWhite, rWhite;
+int lxAt, lxGNDt, rxAt, rxGNDt;
 
 void setup() {
+  lxATime = rxATime = lxGNDTime = rxGNDTime = 0;
   prevTime = currentTime = redTime = greenTime = lYellowTime = rYellowTime = millis();
   red = green = lYellow = rYellow = lWhite = rWhite = false;
+  lxAt = lxGNDt = rxAt = rxGNDt = 0;
+
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
+
+  pinMode(A0, INPUT);  // A line lame left               rosso a line
+  pinMode(A1, INPUT);  // B line left                    nero  b line
+  pinMode(A2, INPUT);  //                                verde gnd
+
+  pinMode(A3, INPUT);  // A line right
+  pinMode(A4, INPUT);  // B line right
+  pinMode(A5, INPUT);  //gnd right
+
   Serial.begin(9600);
   mySerial.begin(2400);  // DATA-LINE Serial type: 2400-N-8-1
-
-  pinMode(A0, INPUT_PULLUP);  // A line left
-  pinMode(A1, INPUT);         // B line left
-
-  pinMode(A4, INPUT_PULLUP);  // A line right
-  pinMode(A5, INPUT);         // B line right
-
-  // C lines to arduino GND
 }
 
 void loop() {
 
   currentTime = millis();
 
-  if ((red == true) && (currentTime > (redTime + 1200))) {
+  if ((currentTime > (lxATime + msHitTime)) && lxATime != 0) {
+    if (lxAt >= msHitTime) {
+      lxAt = 0;
+      if (lYellow == false) {  // DA CONTROLLARE
+        if ((green == true) && (millis() <= greenTime + msDoubleTime)) {
+          red = true;
+          redTime = millis();
+        } else {
+          if (green == false) {
+            red = true;
+            redTime = millis();
+          }
+        }
+      }
+    }
+  }
+
+  if ((currentTime > (rxATime + msHitTime)) && rxATime != 0) {
+    if (rxAt >= msHitTime) {
+      rxAt = 0;
+      if (rYellow == false) {  // DA CONTROLLARE
+        if ((red == true) && (millis() <= redTime + msDoubleTime)) {
+          green = true;
+          greenTime = millis();
+        } else {
+          if (red == false) {
+            green = true;
+            greenTime = millis();
+          }
+        }
+      }
+    }
+  }
+
+  if ((red == true) && (currentTime > (redTime + msHitLampDelay))) {
     red = false;
   }
 
-  if ((lYellow == true) && (currentTime > (lYellowTime + 300))) {
+  if ((lYellow == true) && (currentTime > (lYellowTime + msGNDLampDelay))) {
     lYellow = false;
   }
 
-  if ((green == true) && (currentTime > (greenTime + 1200))) {
+  if ((green == true) && (currentTime > (greenTime + msHitLampDelay))) {
     green = false;
   }
 
-  if ((rYellow == true) && (currentTime > (rYellowTime + 300))) {
+  if ((rYellow == true) && (currentTime > (rYellowTime + msGNDLampDelay))) {
     rYellow = false;
   }
 
   int lxA = analogRead(A0);
   int lxB = analogRead(A1);
+  int lxGND = analogRead(A2);
 
-  int rxA = analogRead(A4);
-  int rxB = analogRead(A5);
+  int rxA = analogRead(A3);
+  int rxB = analogRead(A4);
+  int rxGND = analogRead(A5);
 
-  if (lxB > 1000) {
-    red = true;
-    redTime = millis();
-  }
-  if (lxA + lxB < 30) {
+  // GNDs
+  if ((lxA < aGND && (lxGND > extGND || rxGND > extGND)) || (lxB < v5Drop && (lxGND > extGND || rxGND > extGND)) || lxB < realGND || lxA < realGND) {
     lYellow = true;
     lYellowTime = millis();
   }
-
-  if (rxB > 1000) {
-    green = true;
-    greenTime = millis();
-  }
-  if (rxA + rxB < 30) {
+  if ((rxA < aGND && (rxGND > extGND || lxGND > extGND)) || (rxB < v5Drop && (rxGND > extGND || lxGND > extGND)) || rxB < realGND || rxA < realGND) {
     rYellow = true;
     rYellowTime = millis();
   }
 
-  if (currentTime % 500 == 0) {
+  // HITs
+  if (rxA > supHit) {
+    if (rxAt == 0) rxATime = millis();
+    rxAt++;
+  }
+  if (lxA > supHit) {
+    if (lxAt == 0) lxATime = millis();
+    lxAt++;
+  }
+  
+
+  if (currentTime % 100 == 0) {
     Serial.print(lxA);
     Serial.print(", ");
     Serial.print(lxB);
     Serial.print(", ");
+    Serial.print(lxGND);
+    Serial.print(", ");
     Serial.print(rxA);
     Serial.print(", ");
-    Serial.println(rxB);
+    Serial.print(rxB);
+    Serial.print(", ");
+    Serial.println(rxGND);
   }
 
   /*
+
+  DEBUG ZONE
 
   if (Serial.available() > 0) {
     String in = Serial.readString();
